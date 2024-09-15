23ef7930b564a8c80697cb4cb64d9afff710d2ac
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-15 14:33:22 -0500

    added event listener to the primary btn

---
+++ client/standard/buttons/button.js
@@ -0,0 +1,5 @@
+const primaryBtn = document.createElement('btn-primary')
+const buttoonGroup = document.querySelector('.btn-grou8p') 
+buttoonGroup.appendChild(primaryBtn);
+
+primaryBtn.addEventListener()
