5e5c3a94d04bd28b8990ea9e52605eaa2cb6d57e
Darrell Parkhouse <darrellparkhouse910@gmail.com>
2024-09-17 08:14:08 -0500

    defiend normalizeData and normalizeDataKey, setDataAttribute and removeDataAttribute

---
+++ framework/dom/manipulator.js
@@ -0,0 +1,36 @@
+// todo: use a prefix instead of the standard cl (component-library)
+const normalizeData = (value) => {
+    if (value === 'true')
+        return true
+
+    if (value === 'false')
+        return false
+
+    if (value === Number(value).toString())
+        return Number(value)
+
+    if (value === '' || value === 'null')
+        return null
+
+    if (typeof value !== 'string')
+        return value
+
+    try {
+        return JSON.parse(decodeURIComponent(value))
+    } catch {
+        return value
+    }
+}
+
+const normalizeDataKey = (key) => {
+    return key.replace(/[A-Z]/g, chr => `-${chr.toLowerCase()}`)
+}
+
+const Manipulator = {
+    setDataAttribute: (elem, key, value) => {
+        return key.setAttribute(`data-cl-${normalizeDataKey(key)}`, value)
+    },
+    removeDataAttribute: (elem, key) => {
+        return key.setAttribute(`data-cl-${normalizeDataKey(key)}`)
+    }
+}
